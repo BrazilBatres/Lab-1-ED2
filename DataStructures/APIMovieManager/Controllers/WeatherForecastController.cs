9874014fadbb9cff494ca2d11dc947ff6a2f5258
jglunac <59965122+jglunac@users.noreply.github.com>
2020-09-07 17:09:14 -0600

    Controller

--- DataStructures/APIMovieManager/Controllers/WeatherForecastController.cs
+++ DataStructures/APIMovieManager/Controllers/WeatherForecastController.cs
@@ -8,13 +8,9 @@ using Microsoft.Extensions.Logging;
 namespace APIMovieManager.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/Pruebas")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -23,17 +19,14 @@ namespace APIMovieManager.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("{date}")]
+        
+        public ActionResult Pruebas()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var result = "hola";
+            return Ok(result);
         }
+
+       
     }
 }
